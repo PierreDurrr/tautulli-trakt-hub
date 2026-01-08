756d8d6c396913518de36d2469dabf05ae5215ab
PierreDurrr <73993035+PierreDurrr@users.noreply.github.com>
2026-01-08 11:59:01 +0100

    Update routes.py

--- app/ui/routes.py
+++ app/ui/routes.py
@@ -2,9 +2,16 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from db import SessionLocal, User
+from fastapi.responses import RedirectResponse
+from plex.users import sync_plex_users
 
 templates = Jinja2Templates(directory="ui/templates")
 router = APIRouter()
+
+@router.post("/users/sync")
+def sync_users():
+    sync_plex_users()
+    return RedirectResponse("/", status_code=303)
 
 @router.get("/", response_class=HTMLResponse)
 def dashboard(request: Request):
