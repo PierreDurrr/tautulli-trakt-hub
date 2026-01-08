5d1338a5bd868342cc94dc7b2de6fd723b900603
PierreDurrr <73993035+PierreDurrr@users.noreply.github.com>
2026-01-08 11:58:01 +0100

    Sync Plex users and update database records
    Renamed function to sync_plex_users and added database synchronization logic for Plex users.

--- app/plex/users.py
+++ app/plex/users.py
@@ -1,11 +1,52 @@
 import requests
 import xmltodict
 from config import PLEX_TOKEN
+from db import SessionLocal, User
 
-def fetch_plex_users():
+def sync_plex_users():
+    if not PLEX_TOKEN:
+        raise RuntimeError("PLEX_TOKEN not set")
+
     r = requests.get(
         "https://plex.tv/api/users",
-        headers={"X-Plex-Token": PLEX_TOKEN}
+        headers={"X-Plex-Token": PLEX_TOKEN},
+        timeout=10
     )
+    r.raise_for_status()
+
     data = xmltodict.parse(r.text)
-    return data["MediaContainer"]["User"]
+    plex_users = data["MediaContainer"].get("User", [])
+
+    db = SessionLocal()
+
+    seen_usernames = set()
+
+    for u in plex_users:
+        username = u.get("@username")
+        plex_id = u.get("@id")
+        managed = u.get("@home") == "1"
+
+        if not username:
+            continue
+
+        seen_usernames.add(username)
+
+        user = db.query(User).filter_by(plex_username=username).first()
+        if not user:
+            user = User(
+                plex_username=username,
+                plex_user_id=plex_id,
+                is_managed=managed,
+                enabled=not managed
+            )
+            db.add(user)
+        else:
+            user.plex_user_id = plex_id
+            user.is_managed = managed
+
+    # Disable users removed from Plex
+    for user in db.query(User).all():
+        if user.plex_username not in seen_usernames:
+            user.enabled = False
+
+    db.commit()
